72de2a1b7d60abeef66d622d0adbfefae096e258
abarna <abarna_p@outlook.com>
2022-11-17 16:04:24 +0530

    Newly created menu.css file and modified menu.php

--- menu.php
+++ menu.php
@@ -5,33 +5,117 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Menu</title>
+    <link rel="stylesheet" href="menu.css">
 </head>
 <body>
+<div class="nav-bar">
+       
+       <ul>
+           <li> <h2 class="left">Restaurantly</h2></li>
+           <div class="right">
+          
+               <li><a  href="#home">Home</a></li>
+               <li><a href="about.php">About</a></li>
+               <li><a href="chef.php">Chefs</a></li>
+               <li><a href="gallery.php">Gallery</a></li>
+               <li><a href="contact.php">Contact Us</a></li>
+               <li><a href="admin.php">Admin</a></li>
+               
+               
+           </div>
+           
+       </ul>
+   </div>
     <div class="container">
         <div class="header">
             <h1>Check Our Tasty Menu</h1>
         </div>
         <div class="special">
             <ul>
-                <li>All</li>
-                <li>Starters</li>
-                <li>Salads</li>
-                <li>Specialty</li>
+                <li><a  href="#all">All</a></li>
+                <li><a  href="#s1">Starters</a></li>
+                <li><a  href="#s2">Salads</a></li>
+                <li><a  href="#s3">Specialty</a></li>
             </ul>
         </div>
-
-        <div class="">
-            <div class=" menu-item ">
-                <img src="image/lobster-bisque.jpg" class="menu-img" alt="">
+    <section class="img-section">
+        <div class="menu-column">
+            <div class="menu-item ">
+                <img src="image/lobster-bisque.jpg" class="menu-img" alt="" style="width:80px">
                 <div class="menu-content">
-                <a href="#">Lobster Bisque</a><span>$5.95</span>
+                <h5 id="s1">Lobster Bisque</h5><span>$5.95</span>
+                <p>Lorem, deren, trataro, filede, nerada<p>
                 </div>
-                <div class="menu-ingredients">
-                Lorem, deren, trataro, filede, nerada
+            </div>
+            <div class="menu-item">
+                <img src="image/bread-barrel.jpg" class="menu-img" alt="" style="width:80px">
+                <div class="menu-content">
+                <h5 id="s3">Bread Barrel</h5><span>$6.95</span>
+                <p>Lorem, deren, trataro, filede, nerada<p>
+                </div>
+          </div>
+        </div>
+        <div class="menu-column">
+          <div class=" menu-item ">
+                <img src="image/cake.jpg" class="menu-img" alt="" style="width:80px">
+                <div class="menu-content">
+                <h5 id="s1">Crab Cake</h5><span>$7.95</span>
+                <p>A delicate crab cake served on a toasted roll with lettuce and tartar sauce<p>
+                </div>
+          </div>
+
+          <div class=" menu-item ">
+                <img src="image/caesar.jpg" class="menu-img" alt="" style="width:80px">
+                <div class="menu-content">
+                <h5 id="s2">Caesar Selections</h5><span>$8.95</span>
+                <p>Lorem, deren, trataro, filede, nerada</p>
+                </div>
+          </div>
+        </div>
+        <div class="menu-column">
+          <div class=" menu-item ">
+                <img src="image/tuscan-grilled.jpg" class="menu-img" alt="" style="width:80px">
+                <div class="menu-content">
+                <h5 id="s3">Tuscan Grilled</h5><span>$9.95</span>
+                <p>Grilled chicken with provolone, artichoke hearts, and roasted red pesto</p>
+                </div>
+          </div>
+
+          <div class=" menu-item ">
+                <img src="image/mozzarella.jpg" class="menu-img" alt="" style="width:80px">
+                <div class="menu-content">
+                <h5 id="s1">Mozzarella Stick</h5><span>$4.95</span>
+                <p>Lorem, deren, trataro, filede, nerada</p>
                 </div>
             </div>
         </div>
+        <div class="menu-column">
+          <div class="menu-item ">
+                <img src="image/greek-salad.jpg" class="menu-img" alt="" style="width:80px">
+                <div class="menu-content">
+                <h5 id="s2">Greek Salad</h5><span>$9.95</span>
+                Fresh spinach, crisp romaine, tomatoes, and Greek olives
+                </div>
+          </div>
 
-    </div>
+          <div class="menu-item ">
+                <img src="image/spinach-salad.jpg" class="menu-img" alt="" style="width:80px">
+                <div class="menu-content">
+                <h5 id="s2">Spinach Salad</h5><span>$9.95</span>
+                <p>Fresh spinach with mushrooms, hard boiled egg, and warm bacon vinaigrette</p>
+                </div>
+          </div>
+        </div>
+        <div class="menu-column">
+           <div class="menu-item">
+                <img src="image/lobster-roll.jpg" class="menu-img" alt="" style="width:80px">
+                <div class="menu-content">
+                <h5 id="s3">Lobster Roll</h5><span>$12.95</span>
+                <p>Plump lobster meat, mayo and crisp lettuce on a toasted bulky roll</p>
+                </div>
+          </div>
+        </div>
+    </section>
+</div>
 </body>
 </html>
